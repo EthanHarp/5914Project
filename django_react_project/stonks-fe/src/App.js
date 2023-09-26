368433097c5228844967f12f64e7c63c5358a408
Codin Kid <74441312+CodinKid@users.noreply.github.com>
2023-09-26 14:44:15 -0400

    Added initial frontend code to connect to backend

--- django_react_project/stonks-fe/src/App.js
+++ django_react_project/stonks-fe/src/App.js
@@ -1,13 +1,35 @@
 import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/get_data/') // Replace with your actual API endpoint
+        .then((response) => {
+            setData(response.data);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+  }, []);
+
   return (
     <div className="container">
       <h1>Stonks</h1>
       <Form.Control type="text" placeholder="Enter stock" />
+      {/* Render your data in your component */}
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
