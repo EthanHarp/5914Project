538cde2dc73199d93153677d3e008a2ae4afba84
Codin Kid <74441312+CodinKid@users.noreply.github.com>
2023-09-26 16:57:28 -0400

    Added images to frontend

--- django_react_project/stonks-fe/src/App.js
+++ django_react_project/stonks-fe/src/App.js
@@ -3,6 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+// Images
+import giveUp from './assets/give-up.png'
+import keepMining from './assets/keep-mining.png'
+import memeMan from './assets/Meme Man Outlined HD.png'
+
+function determineImg(goodStock) {
+  if (goodStock) {
+    return keepMining
+  } else {
+    return giveUp
+  }
+}
 
 function App() {
   const [data, setData] = useState([]);
@@ -19,10 +31,18 @@ function App() {
       });
   }, []);
 
+  // Update this later to change depending on stock status
+  let goodStock = true
+  let currentImg = determineImg(goodStock)
+
   return (
     <div className="container">
+      <img class="App-logo" src={memeMan} alt="Meme Man" />
       <h1>Stonks</h1>
       <Form.Control type="text" placeholder="Enter stock" />
+      <div>
+        <img src={currentImg} alt="mining" />
+      </div>
       <div>
         {JSON.stringify(data)}
       </div>
