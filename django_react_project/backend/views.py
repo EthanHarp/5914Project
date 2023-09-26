43d544f9eaffd8488848ec0cbc569d964aaafe0c
Codin Kid <74441312+CodinKid@users.noreply.github.com>
2023-09-26 16:24:17 -0400

    Got frontend to display backend data

--- django_react_project/backend/views.py
+++ django_react_project/backend/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import os
 
 def get_data(request):
-    # Read the JSON file and parse its contents
-    with open('data/response1.json', 'r') as json_file:
-        data = json.load(json_file)
+    # # Read the JSON file and parse its contents
+    # with open('./data/response1.json', 'r') as json_file:
+    #     data = json.load(json_file)
+
+        # Get the directory where views.py is located
+    current_directory = os.path.dirname(__file__)
+    
+    # Construct the path to response1.json within your_app
+    file_path = os.path.join(current_directory, 'data', 'response1.json')
+
+    try:
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        # Handle other exceptions (e.g., JSON decoding error)
+        print("Exception occurred")
 
     return JsonResponse(data)
--- django_react_project/stonks-fe/src/App.js
+++ django_react_project/stonks-fe/src/App.js
@@ -1,35 +1,32 @@
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/get_data/') // Replace with your actual API endpoint
-        .then((response) => {
-            setData(response.data);
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
+    axios
+      .get('http://localhost:8000/api/get_data/')
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   return (
     <div className="container">
       <h1>Stonks</h1>
       <Form.Control type="text" placeholder="Enter stock" />
-      {/* Render your data in your component */}
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <div>
+        {JSON.stringify(data)}
+      </div>
     </div>
-    
   );
 }
 
