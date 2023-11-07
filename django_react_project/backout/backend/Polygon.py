d0a923f0d4f79d24085a62aa36f951e7c694551f
Bhargav Ganesh <ganesh.85@osu.edu>
2023-11-07 15:11:30 -0500

    Added the Polygon API calling methods for cleaning data

--- django_react_project/backout/backend/Polygon.py
+++ django_react_project/backout/backend/Polygon.py
@@ -3,6 +3,8 @@ import random
 import json
 from datetime import date
 from datetime import timedelta
+from bs4 import BeautifulSoup
+import os
 
 API_KEYS = ["QcM3lMmWYppCbhCPm9n1KLwJGKy3RLb1", "viTVaYWlzaYq9WApiI4xbHKpowbnTTpT", "RVi3LLrhTtCliT_8Tqlm3R1afDMKZXNo", "bMpmXrDdKpyLjRdMxjSeP_JxH8IHPE9j"]
 
@@ -22,29 +24,51 @@ def getDailyOpenClose(ticker, date=str(date.today() - timedelta(days = 1))):
         return {}
     else:
         return response
+    
+def getArticle(article):
+    url = article['article_url']
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    article_content = ""
+    for p in soup.find_all("p"):
+        article_content += p.get_text()
+    return article_content    
 
-def getNewsForTicker(ticker, order = "desc", limit = "1000"):
-    API_URL = "https://api.polygon.io/v2/reference/news?ticker="+ticker.upper()+"&order="+order+"&limit="+limit+"&apiKey="+random.choice(API_KEYS)+""
-    response = requests.get(API_URL).json()
-    #dumpJSON(response, ticker+".json")
-    #return response
-    if (response["status"] == "NOT_FOUND"):
-        return {}
-    else:
-        return response["results"]
+def cleanData():
+    for fName in os.listdir('data/'):
+        with open("data/"+fName, "r") as file:
+            print("Cleaning data for " + fName[0:-4])
+            response = json.load(file)
+            articles = response['results']
+            response.clear()
+            for article in articles:
+                title = article['title']
+                description = article.get('description', '')
+                link = article['article_url']
+                article_content = getArticle(article)
+                article.clear()
+                article['title'] = title
+                article['description'] = description + " ".join(article_content.split())
+                article['link'] = link
+            response['articles'] = articles
+        json_object = json.dumps(response, indent = 4)
+        open("data/"+fName, "w").close()
+        with open("data/"+fName, "w") as file:
+            file.write(json_object)
 
-# def getNewsForTickers():
-#     TICKERS = ["AAPL", "TSLA", "NVDA", "AMZN", "MSFT", "AMD", "BYND", "META", "GOOGL", "MCD"]
+def getNewsForTickers():
+    TICKERS = ["AAPL", "TSLA", "NVDA", "AMZN", "MSFT", "AMD", "BYND", "META", "GOOGL", "MCD"]
 
-#     for ticker in TICKERS:
-#         print("Getting news for " + ticker)
-#         API_URL = "https://api.polygon.io/v2/reference/news?ticker="+ticker+"&order=desc&limit=1000&apiKey="+random.choice(API_KEYS)+""
-#         response = requests.get(API_URL).json()
-#         dumpJSON(response, ticker+".json")
+    for ticker in TICKERS:
+        print("Getting news for " + ticker)
+        API_URL = "https://api.polygon.io/v2/reference/news?ticker="+ticker+"&order=desc&limit=100&apiKey="+random.choice(API_KEYS)+""
+        response = requests.get(API_URL).json()
+        dumpJSON(response, ticker+".json")
 
-# def dumpJSON(response, fName = "sample.json"):
-#     json_object = json.dumps(response, indent = 4)
-#     with open("data/"+fName, "w") as outfile:
-#         outfile.write(json_object)
+def dumpJSON(response, fName = "sample.json"):
+    json_object = json.dumps(response, indent = 4)
+    with open("data/"+fName, "w") as outfile:
+        outfile.write(json_object)
 
-# getNewsForTickers()
+getNewsForTickers()
+cleanData()
