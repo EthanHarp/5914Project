3a7dd84591f44e32c407501535a6550ac342b74c
BuckGuy <landis.235>
2023-11-30 15:43:47 -0500

    Sample response sending (not elasticsearch)

--- django_react_project/backout/backend/views.py
+++ django_react_project/backout/backend/views.py
@@ -21,9 +21,15 @@ def get_data(request, ticker):
         #}
 
         #return JsonResponse(combined_response)
-   # except Exception as e:
-    print("get data")
+    # except Exception as e:
+    print("get data", ticker)
     #    return JsonResponse({"error": str(e)}, status=500)
+    sample_data = {
+            "title": "Here is Why Growth Investors Should Buy Meta Platforms (META) Now",
+            "description": "Meta Platforms (META) possesses solid growth attributes, which could help it handily outperform the market.",
+            "link": "https://www.zacks.com/stock/news/2184848/here-is-why-growth-investors-should-buy-meta-platforms-meta-now"
+        }
+    return(JsonResponse(sample_data))
 
 def get_data_old(request, ticker):
     # # Read the JSON file and parse its contents
@@ -44,10 +50,6 @@ def get_data_old(request, ticker):
     except Exception as e:
         # Handle other exceptions (e.g., JSON decoding error)
         print("Exception occurred")
-
-
-    
-
 
     return JsonResponse(getDailyOpenClose(ticker), safe=False)
     #return HttpResponse(f'The parameter is: {ticker}')
